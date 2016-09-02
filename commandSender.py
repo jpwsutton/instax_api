bc137e4d8334c7efbd4028619cdd53a6d9b30190
James Sutton <james.sutton@uk.ibm.com>
2016-09-02 15:53:55 +0100

    Some more changes for the command tester

--- commandSender.py
+++ commandSender.py
@@ -4,6 +4,11 @@
 A simple test client
 """
 import socket
+import sys
+
+
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
 
 host = ''
 port = 8080
@@ -12,34 +17,39 @@ print("Instax API Command Sender")
 
 def printByteArray(byteArray):
     hexString = ''.join('%02x'%i for i in byteArray)
-    print(' '.join(hexString[i:i+4] for i in range(0, len(hexString), 4)))
+    return(' '.join(hexString[i:i+4] for i in range(0, len(hexString), 4)))
 
 
-def sendCommand(commandBit, password):
+def generatePayload(commandBit, mysteryInt, payloadArray, password):
     payload = bytearray() # The Command Packet is always 12 bytes
     payload.append(36 & 0xFF) # We always start with 36
     payload.append(commandBit & 0xFF) # Command Bit?
-    payload.append(0) # Something else
-    payload.append(0) # Something else
-    payload.append(0) # Something else
-    payload.append(0) # Something else
-    payload.append(0) # Something else
-    payload.append(0) # Something else
-    payload.append((password >> 8) & 255) # Password Shifted by 8
-    payload.append(password & 255) # Password
+    payload.append((mysteryInt >> 8) & 0xFF) # Something else
+    payload.append(mysteryInt & 0xFF) # Something else
+    payload.append((payloadArray[0] >> 24) & 0xFF) # Something else
+    payload.append((payloadArray[1] >> 16) & 0xFF) # Something else
+    payload.append((payloadArray[2] >> 8) & 0xFF) # Something else
+    payload.append(payloadArray[3] & 0xFF) # Something else
+    payload.append((password >> 8) & 0xFF) # Password Shifted by 8
+    payload.append(password & 0xFF) # Password
     payload.append(0) # Append Nothing
     payload.append(0) # Append Nothing
-    printByteArray(payload)
+    print('Sending:  ' + printByteArray(payload))
     return payload
 
-myPayload = sendCommand(192, 1111)
+def sendAndRecv(payload):
+    sock.sendall(payload)
+    return(sock.recv(1024))
+
 #Create a TCP/IP Socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((host, port))
 
+
+
 try:
-    sock.sendall(myPayload)
-    print(sock.recv(1024))
+    response = sendAndRecv(generatePayload(192,50,[12,12,12,12],1111))
+    print('Received: ' + printByteArray(response))
 
 finally:
     print('Closing Socket')
