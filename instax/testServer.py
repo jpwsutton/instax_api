945a57f9557ac6a0f5797de8f8a75a2c21a7e47f
James Sutton <jpwsutton@gmail.com>
2017-08-14 20:50:33 +0100

    Making testServer.py pep8 compliant

--- instax/testServer.py
+++ instax/testServer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import socket
 from .commands import Commands
-from .utilities  import Utilities
+from .utilities import Utilities
 from .packet import Packet
+
 
 class TestServer:
     """ A Test Server for the Instax Library,
@@ -17,30 +18,30 @@ class TestServer:
         self.backlog = 5
         self.packetFactory = PacketFactory()
 
-
     def start(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((self.host, self.port))
         self.socket.listen(self.backlog)
         print('Listening on %s port %s' % (self.host, self.port))
-        while 1:
+        while True:
             client, address = self.socket.accept()
-            print('-----------------------------------------------------------')
+            print('----------------------------------------------------------')
             header_data = client.recv(4)
-            msg_len = ((header_data[2] &0xFF) << 8 | (header_data[3] &0xFF) << 0)
-            print('message length: ',  msg_len)
+            msg_len = ((header_data[2] & 0xFF) << 8 |
+                       (header_data[3] & 0xFF) << 0)
+            print('message length: ', msg_len)
             if msg_len:
                 data = client.recv(msg_len - 4)
                 payload = header_data + data
                 client.send(payload)
                 print('received: %s' % self.printByteArray(payload))
                 packet = self.packetFactory.getPacket(payload)
-                print('-----------------------------------------------------------')
-            #client.close()
-
+                print('---------------------------------------------------')
+            # client.close()
 
     def printByteArray(self, byteArray):
-        hexString = ''.join('%02x'%i for i in byteArray)
-        return(' '.join(hexString[i:i+4] for i in range(0, len(hexString), 4)))
+        hexString = ''.join('%02x' % i for i in byteArray)
+        return(' '.join(hexString[i:i + 4] for i in range(
+                0, len(hexString), 4)))
 
-    #def generateResponse(self, cmdBit, time)
+    # def generateResponse(self, cmdBit, time)
